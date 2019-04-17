a11d879606e896a888051ce5f2e157619270b3e8
Serena Lertkantitham <serena.lertkantitham@gmail.com>
2019-04-16 18:37:09 -0700

    boilerplate action and reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,16 @@
 import * as actions from "../actions";
 
-const initialState = {};
+const initialState = {
+  guesses: [1, 52, 34, 98],
+  feedback: "test feedback",
+  auralStatus: "test auralStatus",
+  correctAnswer: 23
+};
 
-export const HotColdReducer = (state = initialState, action) => {};
+export const HotColdReducer = (state = initialState, action) => {
+  if (action.type === actions.MAKE_GUESS) {
+  } else if (action.type === actions.RESTART_GAME) {
+  } else {
+    return state;
+  }
+};
